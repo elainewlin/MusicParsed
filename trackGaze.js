d96c5d3b4d4304834f1c2745453cf77fbcd33fe5
Phoebe Tse <phoebet@mit.edu>
2017-04-14 10:47:14 -0400

    gaze tracking is not lagging anymore

--- trackGaze.js
+++ trackGaze.js
@@ -1,9 +1,12 @@
-var GAZE_DEBUG = false;
+var GAZE_DEBUG = true;
 var windowHeight = $(window).height();
 var cutoff = 0.6; // set parameter to determine when to scroll down
 
 window.onload = function() {
-  var gazeEngine = webgazer.setGazeListener(function(data, clock) {
+  var gazeEngine = webgazer
+    .setRegression('ridge') /* currently must set regression and tracker */
+    .setTracker('clmtrackr') /* otherwise, it's jumpy */
+    .setGazeListener(function(data, clock) {
     if (data == null) {
       return;
     }
@@ -12,23 +15,10 @@ window.onload = function() {
     var y = data.y; 
 
     if (y > windowHeight * cutoff) {
-      console.log("scroll!")
       smallScroll();
     }
   }).begin();
 
   if (GAZE_DEBUG)
     gazeEngine.showPredictionPoints(true); /* shows a square every 100 milliseconds where current prediction is */
-
-
-// TODO: call this somewhere?
-    // function checkIfReady() {
-    //     if (webgazer.isReady()) {
-    //         console.log('ready')
-    //         return
-    //     } else {
-    //         setTimeout(checkIfReady, 100);
-    //     }
-    // }
-    // setTimeout(checkIfReady,100);
 };
