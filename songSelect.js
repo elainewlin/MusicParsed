cb0ae13dd2f1071e5968c185b9083b72da6418db
Elaine Lin <elin1@mit.edu>
2017-04-21 10:31:48 -0400

    Added HTML parsing

--- songSelect.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $("#song").load("yellowSubmarine.html");
-    $("#yellowSubmarine").click(function() {
-        $("#song").load("yellowSubmarine.html");
-    })
-    $("#loveStory").click(function() {
-        $("#song").load("loveStory.html");
-    })
-})
