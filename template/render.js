a4b288f962cdbf6838b88e1a3fdafc42677b6912
Elaine Lin <elin1@mit.edu>
2017-04-24 11:47:11 -0400

    Added chord images

--- template/render.js
+++ template/render.js
@@ -1,9 +1,11 @@
 window.onload = function() {
   //Grab the inline template
   var template = document.getElementById('template').innerHTML;
+  var chordTemplate = document.getElementById('chordTemplate').innerHTML;
 
   //Parse it (optional, only necessary if template is to be used again)
   Mustache.parse(template);
+  Mustache.parse(chordTemplate);
 
   var allSongs = ["Set Fire to the Rain - Adele", "Love Story - Taylor Swift", "Monster - Imagine Dragons", "Wonderwall - Oasis"];
 
@@ -13,11 +15,9 @@ window.onload = function() {
       var file = ui.item.label + ".json";
       $.getJSON("/template/json/"+file, function(data) {
 
-        //Render the data into the template
-        var rendered = Mustache.render(template, data);
-
-        //Overwrite the contents of #target with the rendered HTML
-        document.getElementById('song').innerHTML = rendered;
+        //Overwrite the contents of song with the rendered HTML
+        document.getElementById('song').innerHTML = Mustache.render(template, data);
+        document.getElementById('chordPics').innerHTML = Mustache.render(chordTemplate, data);
       });
     }
   }); 
