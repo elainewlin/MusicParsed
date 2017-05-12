26e7a063ee028220fc521c24e6b9b0701aa783b7
Elaine Lin <elin1@mit.edu>
2017-05-12 19:20:09 -0400

    Add guitar chords and chord toggle view.

--- view.js
+++ view.js
@@ -1,15 +1,38 @@
 $(document).ready(function() {
-    $("#chordToggle").click(function() {
+
+    function toggleChords() {
         $("#chordPics").toggle(); 
+
+        if($("#chordPics").is(":visible")) {
+            $("#chordToggle").text("hide");
+        }
+        else {
+            $("#chordToggle").text("show");
+        }
+    }
+
+    $("#chordToggle").click(function() {
+       toggleChords();
     })
 
     $("#chordPics").click(function() {
-        $("#chordPics").toggle(); 
+        toggleChords();
     })
 
-   $("#viewToggle").click(function() {
+    $("#viewToggle").click(function() {
         $("#column-count").toggle(); 
         $("#transpose").toggle(); 
+    })
+    $("#instrumentToggle").click(function() {
+        var currentInstrument = $("#song").data()["instrument"];
+        var instruments = ["guitar", "ukulele"];
+
+        var newInstrument = instruments[(instruments.indexOf(currentInstrument) + 1) % instruments.length];
+
+        data = {}
+        data["allChords"] = $("#song").data()["allChords"];
+        data["instrument"] = newInstrument;
+        renderChords(data);
     })
 
     $("#column-count").find("input").click(function(e) {
