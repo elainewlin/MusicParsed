f65575490fd71d2813cbabcae269512755ba7a05
Jake Peterson <jakewp11@gmail.com>
2017-07-05 16:04:13 -0500

    Now sorting all alphabetically
    Artists are sorted alphabetically and Song Titles are alphabetical within the artist list.

--- js/showAllSongs.js
+++ js/showAllSongs.js
@@ -16,6 +16,17 @@ window.onload = function() {
 
         if(allSongs.hasOwnProperty(artist)) {
           allSongs[artist].push(song);
+		  
+		  
+		  allSongs[artist] = allSongs[artist].sort(function(a, b){
+			var nameA=a.title, nameB=b.title;
+				if (nameA < nameB) //sort string ascending
+					return -1;
+				if (nameA > nameB)
+					return 1;
+				return 0; //default return value (no sorting)
+			});
+		  
         }
         else {
           allSongs[artist] = [song];
