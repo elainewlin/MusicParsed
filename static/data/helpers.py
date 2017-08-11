321926b7b1f3260c0bbe029a0ada7c4c1b7deb43
Elaine Lin <elaine@yelp.com>
2017-08-11 12:10:11 -0700

    Adding flask

---
+++ static/data/helpers.py
@@ -0,0 +1,21 @@
+# Helper functions for handling song data
+
+"""
+title + artist <-> songID <-> filename (JSON, text)
+data           <-> songID <-> name
+"""
+
+# name -> songID
+def nameToID(fileName):
+    return fileName.split(".")[0]
+
+# songID -> data
+def idToData(songID):
+    return songID.split(" - ")
+
+# data -> songID -> name
+def dataToName(title, artist, fileType):
+    return "{0} - {1}.{2}".format(title, artist, fileType)
+
+def clean(string):
+    return string.lower().replace("'", "").replace(" ", "_")
