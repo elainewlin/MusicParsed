6c3af2feeee8841641001f83a3374b5dd0ef0d83
Elaine Lin <elin1@mit.edu>
2017-09-28 16:43:35 -0400

    Fixing broken songs

--- static/data/isChord.py
+++ static/data/isChord.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import re
 
 
@@ -5,7 +6,7 @@ def isLabel(line):
     # Checks whether a line is a label
     return line.startswith('[') and line.endswith(']') or line.endswith(':')
 
-pitch = r'[A-G][#b]?'
-chord = pitch + '(?:maj|m|aug|dim)?\d*(?:(?:add|sus|[#b])\d+)*(?:/' + pitch + ')?'
+pitch = r'[A-G](?:bb|𝄫|b|♭|#|♯|x)?'
+chord = pitch + '(?:maj|m|aug|dim)?\d*(?:(?:add|sus|bb|𝄫|b|♭|#|♯|x|𝄪)\d+)*(?:/' + pitch + ')?'
 isChord = re.compile(chord + r'\Z').match
 isChordLine = re.compile(r'\s*(?:' + chord + r'\s+)*' + chord + r'\Z').match
