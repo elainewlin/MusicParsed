5bfaa60d5492ce0c2ba78ebc3520dc893af3dfdc
Elaine Lin <elaine@yelp.com>
2018-05-27 16:51:15 -0700

    db boilerplate code

---
+++ models.py
@@ -0,0 +1,11 @@
+from app import db
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+
+    def __repr__(self):
+        return '<User %r>' % (self.nickname)
