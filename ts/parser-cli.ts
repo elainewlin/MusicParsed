fcc3d99d13eb2a676018a94c2a1e7a0d888694b6
Anders Kaseorg <andersk@mit.edu>
2019-04-03 20:40:16 -0700

    Port parser to TypeScript
    (Mostly written by Elaine.)
    
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

---
+++ ts/parser-cli.ts
@@ -0,0 +1,63 @@
+/* eslint no-console: "off" */
+
+import { parseLines } from "./parser";
+import fs from "fs";
+import path from "path";
+
+const textDir = path.resolve(__dirname, "..", "static", "data", "text");
+const jsonDir = path.resolve(__dirname, "..", "static", "data", "json");
+
+const fields = [
+  "allChords",
+  "artist",
+  "fullName",
+  "id",
+  "lines",
+  "title",
+  "label",
+  "chord",
+  "lyrics",
+];
+
+function compare(a: unknown, b: unknown, path: string): void {
+  if (typeof a !== typeof b) {
+    console.log("wrong type", path, typeof a, typeof b);
+    return;
+  }
+  if (typeof a === "object" && typeof b === "object") {
+    const a1 = a as { [key: string]: unknown };
+    const b1 = b as { [key: string]: unknown };
+    for (let key of new Set([...Object.keys(a1), ...Object.keys(b1)])) {
+      compare(a1[key], b1[key], `${path}.${key}`);
+    }
+  } else if (typeof a === "string" && typeof b === "string") {
+    if (a !== b) {
+      console.log("wrong string", path, a, b);
+    }
+  } else {
+    console.log("what even are these?", path);
+  }
+}
+
+fs.readdirSync(textDir).map(filename => {
+  const m = /^(.*) - (.*)\.txt$/.exec(filename);
+  if (m) {
+    const [, title, artist] = m;
+
+    const songText = fs.readFileSync(path.resolve(textDir, filename), {
+      encoding: "utf-8",
+    });
+    const songData = parseLines({ title, artist, songText });
+
+    const jsonPath = path.resolve(jsonDir, `${songData.id}.json`);
+    const jsonData = JSON.parse(
+      fs.readFileSync(jsonPath, { encoding: "utf-8" })
+    );
+
+    if (JSON.stringify(songData, fields) !== JSON.stringify(jsonData, fields)) {
+      console.log(`Differences in ${songData.id}`);
+      compare(songData, jsonData, "");
+      console.log();
+    }
+  }
+});
---
+++ ts/parser.ts
@@ -0,0 +1,118 @@
+import { SongData } from "./model";
+
+const clean = function(text: string): string {
+  text = text.replace(/[^A-Za-z0-9 ]+/g, "").toLowerCase();
+  return text.replace(/ /g, "_");
+};
+
+const isLabel = function(line: string): boolean {
+  if (!line) {
+    return false;
+  }
+  // Checks whether a line is a label
+  return (line.startsWith("[") && line.endsWith("]")) || line.endsWith(":");
+};
+
+const isChordLine = function(line: string): boolean {
+  if (!line) {
+    return false;
+  }
+
+  const pitch = "[A-G](?:bb|𝄫|b|♭|#|♯|x)?";
+  const fancyChordEnd = "(_[0-9]+)?$";
+  const chordType = `(?:maj|m|aug|dim)?[0-9]*(?:(?:add|sus|no|bb|𝄫|b|♭|#|♯|x|𝄪)[0-9]+)*(?:/${pitch})?`;
+  // We use this when we override chord fingerings for ~fancy~ chords
+  const chord = `^${pitch}${chordType}${fancyChordEnd}`;
+  const chordReg = new RegExp(chord, "g");
+  const chordBoundary = /\S+/g;
+
+  let isLineChord = true;
+
+  line.replace(chordBoundary, function(word) {
+    let wordIsChord = false;
+
+    if (word.match(chordReg)) {
+      wordIsChord = true;
+    }
+
+    isLineChord = isLineChord && wordIsChord;
+    return "";
+  });
+  return isLineChord;
+};
+
+const isLyricLine = function(line: string): boolean {
+  return !isLabel(line) && !isChordLine(line);
+};
+
+export const parseLines = function({
+  title,
+  artist,
+  songText,
+}: {
+  title: string;
+  artist: string;
+  songText: string;
+}): SongData {
+  const data: SongData = {
+    title,
+    artist,
+    fullName: `${title} - ${artist}`,
+    id: `${clean(title)} - ${clean(artist)}`,
+    lines: [],
+    allChords: [],
+  };
+
+  const updateAllChords = function(line: string): void {
+    line.split(" ").map((chord: string) => {
+      if (chord.includes("/")) {
+        chord = chord.split("/")[0];
+      }
+      if (!data.allChords.includes(chord) && chord != "") {
+        data.allChords.push(chord);
+      }
+    });
+  };
+
+  let lines = songText.split("\n");
+  lines = lines.map(line => line.trimRight());
+
+  const iterator = lines[Symbol.iterator]();
+
+  const firstLine = lines[0];
+  if (firstLine.startsWith("CAPO ")) {
+    data.capo = firstLine.slice("CAPO ".length);
+    iterator.next();
+  } else if (firstLine.startsWith("ALL CHORDS ")) {
+    data.overrideAllChords = firstLine.slice("ALL CHORDS ".length).split(";");
+    iterator.next();
+  }
+
+  for (let line of iterator) {
+    if (isLabel(line)) {
+      data.lines.push({ label: line });
+    } else if (isChordLine(line)) {
+      for (;;) {
+        let nextLine = iterator.next().value;
+        let lyrics = "";
+        if (isLyricLine(nextLine)) {
+          lyrics = nextLine;
+        }
+        data.lines.push({ lyrics: lyrics, chord: line });
+        updateAllChords(line);
+
+        line = nextLine;
+        if (isLabel(line)) {
+          data.lines.push({ label: line });
+          break;
+        } else if (!isChordLine(line)) {
+          break;
+        }
+      }
+    } else if (line) {
+      data.lines.push({ lyrics: line, chord: "" });
+    }
+  }
+
+  return data;
+};
