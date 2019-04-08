05fdf6b04ee4380ca2be1581aa5a88ca5057f8b5
Elaine Lin <elainewlin@gmail.com>
2019-04-07 17:58:59 -0700

    Sanity check that instruments have the right number of strings in their chords

---
+++ test/testInstrument.ts
@@ -0,0 +1,17 @@
+import { instrumentsData } from "../lib/instrument";
+import { assert } from "chai";
+
+describe("instrumentsData", () => {
+  it("should sanity-check chords have right number of strings", () => {
+    for (const instrumentData of Object.values(instrumentsData)) {
+      const strings = instrumentData.strings;
+
+      for (const root of instrumentData.chords) {
+        for (const fingering of root.values()) {
+          const fingers = fingering.split(",").length;
+          assert.equal(strings, fingers);
+        }
+      }
+    }
+  });
+});
