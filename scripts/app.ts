c0175c7e3a87e471a88df51471786bad15a3405c
Anders Kaseorg <andersk@mit.edu>
2019-06-02 17:02:32 -0700

    Serve /api/allSongs route from MongoDB
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- scripts/app.ts
+++ scripts/app.ts
@@ -1,12 +1,30 @@
 /* eslint no-console: "off" */
 
+import retry from "async-retry";
+import dotenv from "dotenv";
 import express from "express";
 import fs from "fs";
+import { MongoClient } from "mongodb";
 import nunjucks from "nunjucks";
 import path from "path";
 
+dotenv.config();
 const host = process.env.PORT ? undefined : "127.0.0.1";
 const port = +(process.env.PORT || 5000);
+const mongoUri = process.env.MONGO_URI || "mongodb://localhost:27017";
+const mongoDbName = process.env.MONGO_DB_NAME || "musicparsed";
+
+const dbPromise = (async () => {
+  const mongoClient = await retry(
+    () =>
+      MongoClient.connect(mongoUri, {
+        reconnectTries: Infinity,
+        useNewUrlParser: true,
+      }),
+    { forever: true, maxTimeout: 30000 }
+  );
+  return mongoClient.db(mongoDbName);
+})();
 
 const app = express();
 
@@ -39,6 +57,16 @@ app.use((req, res, next) => {
 });
 
 // Routes
+app.get("/api/allSongs", async (req, res) => {
+  const db = await dbPromise;
+  res.json(
+    await db
+      .collection("songs")
+      .find({}, { projection: { artist: 1, id: 1, tags: 1, title: 1, url: 1 } })
+      .toArray()
+  );
+});
+
 app.use("/static", express.static(path.resolve(__dirname, "../static")));
 
 app.get(["/", "/all"], (req, res) => res.render("all_songs"));
