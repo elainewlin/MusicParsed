c23612e07fa86bdb7389ab73bf974b5ea15f6200
Elaine Lin <elainewlin@gmail.com>
2025-02-20 14:47:16 -0500

    Update the rest of jQuery event handlers.

--- web/index.ts
+++ web/index.ts
@@ -5,7 +5,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "../css/styles.css";
 import "../css/global.css";
 
-$(document).ready(() => {
+$(document).on("ready", () => {
   initRender();
   popStateHandler(window.history);
   window.onpopstate = popStateHandler;
